6f3604bb19cde11238c8627f420c0d74c7b6543e
zichengliao <zichengliao@users.noreply.github.com>
2021-09-04 13:33:51 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -41,6 +41,8 @@ if __name__ == '__main__':
         epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
         visualizer.reset()              # reset the visualizer: make sure it saves the results to HTML at least once every epoch
         #model.update_learning_rate()    # update learning rates in the beginning of every epoch.
+        import warnings
+        warnings.filterwarnings('ignore')
         for i, data in enumerate(dataset):  # inner loop within one epoch
             iter_start_time = time.time()  # timer for computation per iteration
             if total_iters % opt.print_freq == 0:
